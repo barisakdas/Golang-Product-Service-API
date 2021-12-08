6dc2dba21273d421f1212ff50f137729944df05c
barisakdas <39412598+barisakdas@users.noreply.github.com>
2021-12-08 19:19:18 +0300

    database model updated

--- domain/entity/database.go
+++ domain/entity/database.go
@@ -2,11 +2,12 @@ package entity
 
 import (
 	"fmt"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-var Dsn string = "host=localhost user=admin password=Acaba1234!** dbname=hb port=5433 sslmode=disable TimeZone=Asia/Shanghai"
+var Dsn string = "host=localhost user=admin password=Acaba1234!** dbname=product port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 
 func DbMigration() {
 	Campaign{}.Migrate()
@@ -15,10 +16,10 @@ func DbMigration() {
 }
 
 func CreateDb() {
-	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5433 user=admin dbname=postgres password=Acaba1234!** sslmode=disable")
-	db = db.Exec("CREATE DATABASE hb;")
+	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=admin dbname=postgres password=Acaba1234!** sslmode=disable")
+	db = db.Exec("CREATE DATABASE product;")
 	if db.Error != nil {
-		fmt.Println("Unable to create DB test_db, attempting to connect assuming it exists...")
+		fmt.Println("Unable to create DB product, attempting to connect assuming it exists...")
 	}
 	if err != nil {
 
